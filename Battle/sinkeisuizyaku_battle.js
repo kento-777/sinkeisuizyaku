8c67e9652697edfb72fc4874d919b9985c80f36f
kento-777 <kentuuuwork@gmail.com>
2025-10-09 00:46:39 +0900

    対戦モードの実装

--- Battle/sinkeisuizyaku_battle.js
+++ Battle/sinkeisuizyaku_battle.js
@@ -1,7 +1,17 @@
 //ペアの作成
-document.getElementById("matchbtn").addEventListener("click",makePair)
+document.getElementById("matchbtn").addEventListener("click",makePair);
 let cardpair = 0;
 let firstCombine = [];
+let message = "";
+
+//turnがtrueの時、1pのターン
+let turn = true
+
+//プレイヤーの得点
+let point_1p = 0;
+let point_2p = 0;
+document.getElementById("player_1_point").textContent = `${point_1p}pt`;
+document.getElementById("player_2_point").textContent = `${point_2p}pt`;
 
 function makePair() {
   const input_a = document.getElementById("symbolInput_a").value.trim();
@@ -21,6 +31,7 @@ function makePair() {
 document.getElementById("startBtn").addEventListener("click", startGame);
 
 function startGame() {
+  document.querySelector(".player_1").classList.toggle("play_1p")
   if (cardpair == 0) return alert("少なくとも1ペア入力してください！");
   
   // シャッフル
@@ -37,6 +48,7 @@ function startGame() {
     card.classList.add("card");
     card.dataset.symbol = sym;
     
+    //カードをめくる
     card.addEventListener("click", () => {
       if (card.classList.contains("flipped") || flipped.length === 2) return;
       flipCard(card);
@@ -44,10 +56,15 @@ function startGame() {
       flipped.push(card);
       if (flipped.length === 2) {
         checkMatch();
+        document.getElementById("player_1_point").textContent = `${point_1p}pt`;
+        document.getElementById("player_2_point").textContent = `${point_1p}pt`;
       }
     });
     
     gameBoard.appendChild(card);
+
+    //得点の更新
+
   });
   
   function flipCard(card) {
@@ -63,25 +80,60 @@ function startGame() {
     flipped = [];
   }
   
+
   function checkMatch() {
     const [a, b] = flipped;
+
+
     if (firstCombine.findIndex(pair => pair.includes(a.dataset.symbol)) === firstCombine.findIndex(pair => pair.includes(b.dataset.symbol))) {
       matched += 1;
 
-      //css調整
-      function addCss(){
-        a.classList.add("matched")
-        b.classList.add("matched")
+      //得点の加算
+      if (turn == true) {
+        point_1p += 1;
+      } else {
+        point_2p += 1;
+      }
+
+
+      function addCss() {
+        if(turn == true) {
+        a.classList.add("matched_1p")
+        b.classList.add("matched_1p")
+        } else {
+        a.classList.add("matched_2p")
+        b.classList.add("matched_2p")         
+        } 
       }
       setTimeout(addCss, 600)
 
 
       flipped = [];
+
+
       if (matched === cardpair) {
-        setTimeout(() => alert("クリア！おめでとう🎉"), 1800);
+        //勝ったほうのメッセージを書く
+        function writeMessage() {
+          if (point_1p < point_2p) {
+            message = "2pのかち!おめでとう!";
+          } else if (point_1p === point_2p) {
+            message = "ひきわけ！もっかいやろう";
+          } else {
+            message = "1pのかち！おめでとう！";
+          }
+        }
+          writeMessage();
+          setTimeout(() => alert(message), 1800);
       }
     } else {
       setTimeout(unflipCards, 1000);
+      turn = !turn
+      //ポイント更新、ターンチェンジ
+      document.querySelector(".player_1").classList.toggle("play_1p");
+      document.querySelector(".player_2").classList.toggle("play_2p");
+      document.getElementById("player_1_point").textContent = `${point_1p}pt`;
+      document.getElementById("player_2_point").textContent = `${point_2p}pt`;
+
     }
   }
 }
